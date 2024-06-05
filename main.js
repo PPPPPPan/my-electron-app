9e8d6b42cbf63a3a35a96895b7b699503086b816
panshijie <panshijie@aspirecn.com>
2024-06-06 00:04:43 +0800

    basics-config

---
+++ main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow } = require("electron")
+const WinState = require("electron-win-state").default
+const path = require('path')
+
+const createWindow = () => {
+  const winState = new WinState({
+    // defaultWidth: 1000,
+    // defaultHeight: 600,
+    // defaultX: 100,
+    // defaultY: 100,
+  })
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    x: 100,
+    y: 100,
+    ...winState.winOptions, // 自定义窗口状态
+    webPreferences: {
+      preload: path.join(__dirname, './preload/index.js'), // 预加载
+    }
+  })
+  winState.manage(win)
+  win.loadURL('http://localhost:3000')
+
+  // 打开开发者工具
+  win.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  // 激活的时候
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on("window-all-closed", () => {
+  if (process.platform !== 'darwin') app.quit()
+})
