868770b73500f2ee8f7bc8a480c64e9fc590eed2
PPPPPPan <>
2024-06-10 21:07:29 +0800

    updata

--- src/views/list/useIndex.js
+++ src/views/list/useIndex.js
@@ -3,7 +3,7 @@ import { ref, reactive, watch } from 'vue'
 const useIndex = () => {
   const isSelectedIndex = ref(-1)
   const handleClickReadItem = (clickIndex, url) => {
-    isSelectedIndex.value = (isSelectedIndex.value === clickIndex) ? -1 : clickIndex
+    isSelectedIndex.value = clickIndex
     window.open(url, '_blank', "width=1100,height=800")
   }
   return {
